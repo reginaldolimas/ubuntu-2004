f3a6b23e32fd3ce905413022d0da507122f42321
reginaldolimas <79094085+reginaldolimas@users.noreply.github.com>
2022-07-21 14:06:13 -0300

    Add files via upload

--- mapfstab.sh
+++ mapfstab.sh
@@ -1,13 +1,25 @@
 read -p "Nome de Usuario(Rede): " usuario
-read -p "Senha do Usuario(Rede): " senha
+read -s -p "Senha do Usuario(Rede): " senha
 touch /root/$usuario
+#
+echo -e "Salvando informacoes de login no arquivo...."
+#
 echo -e "user=$usuario\npassword=$senha" > /root/$usuario
+echo -e "Criando pasta no /mnt...."
+#
 mkdir -p /mnt/srv_$usuario
+#
 read -p "Ip do servidor(arquivos): " ipserver
 read -p "Nome da Pasta: " pasta
 read -p "Nome do usuario local: " userlocal
+#
+echo -e "Adicionando linha no /etc/fstab...."
+#
 echo -e "//$ipserver/$pasta /mnt/srv_$usuario cifs vers=1.0,uid=$userlocal,gid=$userlocal,users,credentials=/root/$usuario 0 0" >> /etc/fstab
+#
+echo -e "Criando link simbolico para area de trabalho...."
 ln -s /mnt/srv_$usuario /home/$userlocal/Desktop/
+#
 umount -av
 sleep 2s
 mount -av
